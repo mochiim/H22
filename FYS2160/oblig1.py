d4f7176479b6408eac80c66c98a91e3bb2d53b48
mochiim <43903898+mochiim@users.noreply.github.com>
2022-08-29 19:47:43 +0200

    Test

--- FYS2160/oblig1.py
+++ FYS2160/oblig1.py
@@ -4,10 +4,11 @@ import matplotlib.pyplot as plt
 time = [] # [s]
 T_b = [] # Temperature of water inside bodum thermos mug
 T_t = [] # Temperature of water inside Temperfect mug
-
+"""
 with open('termokopper.txt', 'r') as lines:
     for line in lines:
         row = line.split()
         time.append(row[0])
         T_b.append(row[1])
         T_t.append(row[2])
+"""
