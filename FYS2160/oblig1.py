8347d5c8daeb94779f352d3046adf05dccbfae16
mochiim <43903898+mochiim@users.noreply.github.com>
2022-08-31 14:19:07 +0200

    Next: Remove array

--- FYS2160/oblig1.py
+++ FYS2160/oblig1.py
@@ -3,20 +3,21 @@ import matplotlib.pyplot as plt
 import os
 
 time = [] # [s]
-T_b = [] # Temperature of water inside bodum thermos mug
 T_t = [] # Temperature of water inside Temperfect mug
+T_b = [] # Temperature of water inside bodum thermos mug
 
 with open('termokopper.txt', 'r') as lines:
     for line in lines:
         row = line.split()
         time.append(row[0])
-        T_b.append(row[1])
-        T_t.append(row[2])
+        T_t.append(row[1])
+        T_b.append(row[2])
 
 time = np.array(time)
 T_b = np.array(T_b)
 T_t = np.array(T_t)
 
-plt.plot(time, T_b)
-plt.show()
+
+
+print(time.size)
 #print(os.getcwd())
