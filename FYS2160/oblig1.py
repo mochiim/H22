cf9aa5ad88dabee1483a2a169dba00d33a4550eb
mochiim <43903898+mochiim@users.noreply.github.com>
2022-08-31 21:23:12 +0200

    Update oblig1.py

--- FYS2160/oblig1.py
+++ FYS2160/oblig1.py
@@ -9,15 +9,23 @@ T_b = [] # Temperature of water inside bodum thermos mug
 with open('termokopper.txt', 'r') as lines:
     for line in lines:
         row = line.split()
-        time.append(row[0])
-        T_t.append(row[1])
-        T_b.append(row[2])
+        time.append(float(row[0]))
+        T_t.append(float(row[1]))
+        T_b.append(float(row[2]))
+
 
 time = np.array(time)
-T_b = np.array(T_b)
 T_t = np.array(T_t)
+T_b = np.array(T_b)
+
+plt.plot(time, T_t, label = "Temperfect mug")
+plt.plot(time, T_b, label = "Bodum mug")
+plt.xlabel("Time [s]")
+plt.ylabel("Temperature [c]")
+plt.legend()
+plt.savefig("temperature.png")
+plt.show()
 
 
 
-print(time.size)
-#print(os.getcwd())
+print(os.getcwd())
