0c604d44fafc7cbd6678aa1661deeecb35c46d18
mochiim <43903898+mochiim@users.noreply.github.com>
2022-09-03 22:41:35 +0200

    Update oblig1.py

--- FYS2160/oblig1.py
+++ FYS2160/oblig1.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 import scipy.stats as sc
+import random
 
 time = [] # [s]
 T_t = [] # Temperature of water inside Temperfect mug
@@ -14,30 +15,63 @@ with open('termokopper.txt', 'r') as lines:
         T_t.append(float(row[1]))
         T_b.append(float(row[2]))
 
-
 time = np.array(time)
 T_t = np.array(T_t)
 T_b = np.array(T_b)
 
-slope_t, intercept_t, r_t, p_t, se_t = sc.stats.linregress(time, T_t)
-slope_b, intercept_b, r_b, p_b, se_b = sc.stats.linregress(time, T_b)
+slope_t, intercept_t, r_t, p_t, se_t = sc.stats.linregress(time, T_t) # tau Temperfect
+slope_b, intercept_b, r_b, p_b, se_b = sc.stats.linregress(time, T_b) # tau bodum
+
+Ctb = 5
+N = 80000
+nstep = 15*N
+tau = N
+Tt = np.zeros(nstep).transpose()
+Tb = np.zeros(nstep).transpose()
+Tt[0] = 1
+Tb[0] = -1
+Tr = -1
+
+for i in range(1, nstep + 1):
+    r = 4*random.randint(1, 1) - 2
+    DT = Tt[i - 1] - Tb[i - 1]
+    if (r < DT):
+        Tt[i] = Tt[i - 1] - 1/N
+        Tb[i] = Tb[i - 1] + Ctb/N
+    else:
+        Tt[i] = Tt[i-1] + 1/N
+        Tb[i] = Tb[i-1] - Ctb/N
 
 
-fig, (ax1, ax2) = plt.subplots(2)
-ax1.plot(time, T_t, label = "Mug 1")
-ax1.plot(time, slope_t*time + intercept_t)
-ax1.set_ylabel("Temperature [c]")
-plt.legend()
-
-
-ax2.plot(time, T_b, label = "Mug 2")
-ax2.plot(time, slope_b*time + intercept_b)
-ax2.set_xlabel("Time [s]")
-ax2.set_ylabel("Temperature [c]")
-plt.legend()
-#plt.savefig("temperature1.png")
+# Temperature plot over time of both mugs
+"""
+plt.plot(time, T_t, label = "Mug 1")
+plt.plot(time, T_b, label = "Mug 2")
+plt.xlabel("Time [s]", fontsize = 18)
+plt.ylabel("Temperature [°C]", fontsize = 18)
+plt.legend(prop={'size': 12})
+plt.savefig("temp.png")
 plt.show()
+"""
 
+"""
+plt.plot(time, T_t, label = "Mug 1")
+plt.plot(time, slope_t*time + intercept_t, label = rf"Linear regression, $\tau$ = {slope_t}")
+plt.xlabel("Time [s]", fontsize = 18)
+plt.ylabel("Temperature [°C]", fontsize = 18)
+plt.legend(prop={'size': 12})
+plt.savefig("tau_t.png")
+plt.show()
+"""
 
+"""
+plt.plot(time, T_b, label = "Mug 2")
+#plt.plot(time, slope_b*time + intercept_b, label = rf"Linear regression, $\tau$ = {slope_b}")
+plt.xlabel("Time [s]", fontsize = 18)
+plt.ylabel("Temperature [°C]", fontsize = 18)
+plt.legend(prop={'size': 12})
+#plt.savefig("tau_b.png")
+plt.show()
+"""
 
 #print(os.getcwd())
