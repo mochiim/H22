c54414f5dd23cd507730255fd75a7194350b8c5a
mochiim <43903898+mochiim@users.noreply.github.com>
2022-09-04 22:43:44 +0200

    04.09.22 22:43

--- FYS2160/oblig1.py
+++ FYS2160/oblig1.py
@@ -30,38 +30,49 @@ slope_b, intercept_b, r_b, p_b, se_b = sc.stats.linregress(time, T_b) # tau bodu
 
 k = 1 #scc.Boltzmann
 N = int(300) # a suitable numbers of oscillators
-nstep = 800
-q = np.linspace(1, nstep, nstep) # number of energy units
+nsteps = 800
+q = np.linspace(1, nsteps, nsteps) # number of energy units
 dq = (q[-1]-q[0])/(len(q)-1)
 
-omega = np.zeros(nstep) # multiplicity of an Einstein solid
-for i in range(nstep):
+omega = np.zeros(nsteps) # multiplicity of an Einstein solid
+for i in range(nsteps):
     omega[i] = factorial(int(q[i]) + N - 1)/(factorial(int(q[i]))*factorial(N - 1))
 
 S = k*np.log(omega) # entropy
 
-dqdS = np.zeros(nstep)
-for i in range(1, nstep):
+dqdS = np.zeros(nsteps)
+for i in range(1, nsteps):
     dqdS[i] = dq/(S[i] - S[i - 1])
 
 T = dqdS # temperature
 
-Cv = np.zeros(nstep) # heat capacity
-for i in range(1, nstep):
+Cv = np.zeros(nsteps) # heat capacity
+for i in range(1, nsteps):
     Cv[i] = dq/(T[i] - T[i -1])
 
-plt.plot(T, Cv)
-plt.xlabel("Temperature [°C]", fontsize = 18)
-plt.ylabel("Heat capacity [J/K]", fontsize = 18)
-plt.show()
-plt.savefig("q_mot_cv.png")
+Ctb = 5
+N = 80000
+nstep = 15*N
+tau = N
+Tt = np.zeros(nstep)
+Tb = np.zeros(nstep)
+Tt[0] = 1
+Tb[0] = -1
+Tr = -1
 
+for i in range(1, nstep):
+    r = 4*random.uniform(1, 1) - 2
+    DT = Tt[i - 1] - Tb[i - 1]
+    if (r < DT):
+        Tt[i] = Tt[i - 1] - 1/N
+        Tb[i] = Tb[i - 1] + Ctb/N
+    else:
+        Tt[i] = Tt[i-1] + 1/N
+        Tb[i] = Tb[i-1] - Ctb/N
 
-plt.plot(T, S)
-plt.xlabel("Temperature [°C]", fontsize = 18)
-plt.ylabel("Entropy [J/K]", fontsize = 18)
+plt.plot(np.linspace(0, nstep, nstep)/tau, Tt, "r")
+plt.plot(np.linspace(0, nstep, nstep)/tau, Tb, "k")
 plt.show()
-plt.savefig("q_mot_S.png")
 
 # Temperature plot over time of both mugs
 """
@@ -81,18 +92,36 @@ plt.plot(time, slope_t*time + intercept_t, label = rf"Linear regression, $\tau$ 
 plt.xlabel("Time [s]", fontsize = 18)
 plt.ylabel("Temperature [°C]", fontsize = 18)
 plt.legend(prop={'size': 12})
-plt.savefig("tau_t.png")
+#plt.savefig("tau_t.png")
 plt.show()
 """
 
 # Plot tau of Bodun
 """
 plt.plot(time, T_b, label = "Mug 2")
-#plt.plot(time, slope_b*time + intercept_b, label = rf"Linear regression, $\tau$ = {slope_b}")
+plt.plot(time, slope_b*time + intercept_b, label = rf"Linear regression, $\tau$ = {slope_b}")
 plt.xlabel("Time [s]", fontsize = 18)
 plt.ylabel("Temperature [°C]", fontsize = 18)
 plt.legend(prop={'size': 12})
-#plt.savefig("tau_b.png")
+plt.savefig("tau_b.png")
+plt.show()
+"""
+
+# plot T against S
+"""
+plt.plot(T, S)
+plt.xlabel("Temperature [°C]", fontsize = 18)
+plt.ylabel("Entropy [J/K]", fontsize = 18)
+plt.savefig("T_mot_S.png")
+plt.show()
+"""
+
+# plot T against Cv
+"""
+plt.plot(T, Cv)
+plt.xlabel("Temperature [°C]", fontsize = 18)
+plt.ylabel("Heat capacity [J/K]", fontsize = 18)
+plt.savefig("T_mot_cv.png")
 plt.show()
 """
 
--- FYS2160/test.py
+++ FYS2160/test.py
@@ -1,53 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from math import factorial
-"""
-Ctb = 5
-N = 80000
-nstep = 15*N
-tau = N
-Tt = np.zeros(nstep).transpose()
-Tb = np.zeros(nstep).transpose()
-Tt[0] = 1
-Tb[0] = -1
-Tr = -1
 
-for i in range(1, nstep + 1):
-    r = 4*random.randint(1, 1) - 2
-    DT = Tt[i - 1] - Tb[i - 1]
-    if (r < DT):
-        Tt[i] = Tt[i - 1] - 1/N
-        Tb[i] = Tb[i - 1] + Ctb/N
-    else:
-        Tt[i] = Tt[i-1] + 1/N
-        Tb[i] = Tb[i-1] - Ctb/N
-"""
-k = 1
-N = int(300)
-N_len = 800
-q = np.linspace(1,N_len,N_len)
-dq = (q[-1]-q[0])/(len(q)-1)
-
-
-omega = np.zeros(N_len)
-for i in range(0,N_len):
-    omega[i] = factorial(int(q[i])+N-1)/(factorial(int(q[i]))*factorial(N-1))
-S = k*np.log(omega)
-
-dqdS = np.zeros(N_len)
-
-for i in range(1,N_len):
-    dqdS[i] = dq/(S[i]-S[i-1])
-T = dqdS
-
-Cv = np.zeros(N_len)
-for i in range(1,N_len):
-    Cv[i] = dq/(T[i]-T[i-1])
-
-
-plt.plot(q,S,label=r"$N=%g$"%(N_len))
-plt.xlabel("q",FontSize=15)
-plt.ylabel(r"$C_V/Nk$",FontSize=15)
-plt.legend()
-
-plt.show()
+print(np.linspace(0, 100, 101))
