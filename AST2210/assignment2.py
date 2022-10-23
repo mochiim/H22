531383a8930f8bfb32a4513c5fc721249a358c4d
mochiim <43903898+mochiim@users.noreply.github.com>
2022-10-23 21:32:03 +0200

    23.10.2022 21:31

--- AST2210/assignment2.py
+++ AST2210/assignment2.py
@@ -12,4 +12,13 @@ with open (datafile) as infile:
         v.append(eval(separate[0]))
         Ta.append(eval(separate[1]))
 
-print(v)
+def KtoJy(Ta):
+    """
+    Conversion from antenna temperature to flux density, T_A[K] → Sν[Jy],
+    """
+    eta_a = 0 # aperture efficiency
+    Gaminv = 24.4152/eta_a # conversion factor reported on the APEX website [Jy/K^-1]
+    return Ta*Gaminv
+
+plt.plot(v, Ta)
+plt.show()
